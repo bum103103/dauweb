283ca5110dbab799eec98493f19de7bbd448222c
채승지 <cotmdwl1405@gmail.com>
2023-05-08 13:57:20 +0900

    first commit

---
+++ src/main/java/com/dauweb/dauweb/DauwebApplication.java
@@ -0,0 +1,13 @@
+package com.dauweb.dauweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DauwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DauwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dauweb/dauweb/DauwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauweb.dauweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DauwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
